0f8658c2a77baca804d62ce51989cec1eb325a93
elias <eliasfaculdade@yahoo.com.br>
2020-10-07 18:23:48 -0300

    ajustes

--- app_calculo_imc_elias_martins/js/index.js
+++ app_calculo_imc_elias_martins/js/index.js
@@ -16,9 +16,27 @@ function calcularImcTest() {
   var centimetros = Fcentimetros;
   var alturaAoquadrado = (metros * 100 + centimetros) / 100;
   var imc = kilos / alturaAoquadrado ** 2;
-  document.querySelector('.lblResultado').value = imc;
+  document.querySelector('.lblResultado').value = imc.toFixed(1);
 }
 
 function preventSubmit(event) {
   event.preventSubmit();
+  event.preventDefault();
+}
+
+function validar() {
+  //var kilos = form.quilos;
+  var kilos = document.querySelector('quilos');
+  //var metros = form.metros;
+  var metros = document.querySelector('metros');
+  //var centimetros = form.centimetros;
+  var centimetros = document.querySelector('centimetros');
+
+  if (kilos.value == '') {
+    alert('Preencha o campo quilos por favor!');
+    // form.quilos.focus();
+    return;
+  }
+
+  calcularImcTest();
 }
